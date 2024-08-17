22130dd2447d13d55578844d178c75309d5f6fdd
Gustavo G. B. <110747819+OhGusutahu@users.noreply.github.com>
2024-08-17 17:33:22 -0300

    Update cache.c

--- src/cache.c
+++ src/cache.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "memory.h"
 #include "cache.h"
 
 /* Functions:
@@ -8,3 +9,22 @@
  * read_data()
  * write_data()
  */
+
+Processor processors[PROC_SIZE];
+
+Cache initCache() {
+    Cache cache;
+    for(int i = 0; i < CACHE_SIZE; i++) {
+        cache.positions[i].tag = -1;
+        cache.positions[i].state = INVALID;
+        strcpy(cache.positions[i].data, "");
+    }
+}
+
+int read_data(int proc_id, int ram_pos) {
+
+}
+
+int write_data(int proc_id, int ram_pos, char *new_data) {
+
+}
