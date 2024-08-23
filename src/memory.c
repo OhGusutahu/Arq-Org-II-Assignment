c3aa2c16c0f1078e716a96340af7f254ceefdb71
Gustavo G. B. <110747819+OhGusutahu@users.noreply.github.com>
2024-08-22 23:38:18 -0300

    Update memory.c

--- src/memory.c
+++ src/memory.c
@@ -5,15 +5,23 @@
 
 /* Functions:
  * initMemory()
-*/
+**/
 
-Memory initMemory() {
+Memory initMemory(FILE *arq) {
+    // FILE *arq é um arquivo .txt com os dados dos livros da biblioteca
+    // os dados estão na forma <index>|<título>|<autor>|<ano de lançamento>\n
     Memory memory;
-    for(int i = 0; i < RAM_SIZE; i++) {
-        /* Ideia inicial:
-         * Dados da memória serem números, relacionados com os IDs dos livros
-         * Talvez haja blocos com as informações dos livros, como título e autor, para poder modificar
-         * struct RAM sujeita a mudança
-        */
+    char block[BLOCK_SIZE];
+    int index = 0;
+    while((fgets(block, BLOCK_SIZE, arq) != NULL) && (index < RAM_SIZE)) {
+        // como os dados estão separados por '\n', fgets facilmente os grava
+        block[strcspn(block, "\n")] = '\0';
+
+        memory.ram[index].line = index;
+        strncpy(memory.ram[index].data, block, BLOCK_SIZE);
+
+        index++;
     }
+
+    return memory;
 }
