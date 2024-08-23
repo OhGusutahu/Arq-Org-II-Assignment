d901e012463289d2b0ed3ca9ff3e0ce87d2b303d
Gustavo G. B. <110747819+OhGusutahu@users.noreply.github.com>
2024-08-22 23:38:34 -0300

    Update memory.h

--- src/memory.h
+++ src/memory.h
@@ -5,7 +5,7 @@
  * struct memory
  * 
  * memory.c functions declarations
- */
+**/
 
 #define BLOCK_SIZE 64 // tamanho do bloco da RAM
 #define RAM_SIZE 50 // quantidade mínima de posições da memória
@@ -19,9 +19,9 @@ typedef struct {
     RAM ram[RAM_SIZE];
 } Memory; // struct da memória principal
 
-Memory initMemory();
+Memory initMemory(FILE *arq);
 /*
  * Função para inicializar a memória principal
-*/
+**/
 
 #endif // MEMORY_H_INCLUDED
