408a718bb7e6de6f9667dc7a6d00eb3cf39ab281
Gustavo G. B. <110747819+OhGusutahu@users.noreply.github.com>
2024-08-22 23:38:03 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -6,8 +6,24 @@
 
 // Functions if needed
 
-int main() {
-    // declarations, init and whatnot
+int main(int argc, char *argv[]) {
+    
+
+    if (argc == 2) {
+        FILE *arq_dados = fopen(argv[1], "r");
+        if(arq_dados == NULL) {
+        // caso o arquivo não seja encontrado
+        perror(NULL);
+        exit(EXIT_FAILURE);
+        }
+        // smth smth
+
+    } else {
+        fprintf(stderr, "Argumentos incorretos!\n");
+        fprintf(stderr, "Modo de uso:\n");
+        fprintf(stderr, "$ %s nome_arquivo\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 
     return 0;
 }
