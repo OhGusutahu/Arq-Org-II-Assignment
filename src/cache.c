14a30a15683900405b1884444520b4214d9bc86f
Gustavo G. B. <110747819+OhGusutahu@users.noreply.github.com>
2024-08-22 23:37:29 -0300

    Update cache.c

--- src/cache.c
+++ src/cache.c
@@ -8,7 +8,7 @@
  * init_cache()
  * read_data()
  * write_data()
- */
+**/
 
 Processor processors[PROC_SIZE];
 
@@ -19,7 +19,41 @@ Cache initCache() {
         cache.positions[i].state = INVALID;
         strcpy(cache.positions[i].data, "");
     }
+    return cache;
 }
+
+void initProcessors() {
+    for (int i = 0; i < PROC_SIZE; i++) {
+        processors[i].cache = initCache();
+    }
+}
+
+/***************************************/
+// Funções beta, talvez mude
+
+int find_cache_line(Cache *cache, int tag) {
+    // Procura a tag pela cache
+    for (int i = 0; i < CACHE_SIZE; i++) {
+        if (cache->positions[i].tag == tag) {
+            return i;
+        }
+    }
+    return -1; // não foi possível encontrar
+}
+
+int FIFO_index = 0;
+/* Posição para a próxima substituição FIFO
+ * Na implementação, os índices estão sendo usados em ordem, portanto se inicia em 0
+**/
+
+int get_FIFO_index() {
+    int index = FIFO_index;
+    FIFO_index = (FIFO_index + 1) % CACHE_SIZE; // próximo index até CACHE_SIZE
+    return index;
+}
+
+// Fim das funções beta
+/***************************************/
 
 int read_data(int proc_id, int ram_pos) {
 
