45f762fb0ca6f585b74407ab8d19afd930d5df45
Gustavo G. B. <110747819+OhGusutahu@users.noreply.github.com>
2024-09-18 21:21:37 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -14,9 +14,8 @@ void clear_buffer() {
 }
 
 int main(int argc, char *argv[]) {
-    
-
-    if (argc == 2) {
+    char *ret = strrchr(argv[1], '.');
+    if (argc == 2 && strcmp(ret, ".txt") == 0) {
         FILE *arq_dados = fopen(argv[1], "r");
         if(arq_dados == NULL) {
             // caso o arquivo não seja encontrado
@@ -30,7 +29,7 @@ int main(int argc, char *argv[]) {
             int proc_id, ram_pos;
             char operation;
 
-            printf("Escolha uma operacao ('r' = leitura, 'w' = escrita, 'q' = sair): ");
+            printf("Escolha uma operacao ('r' = leitura, 'w' = escrita, 'p' = printar, 'q' = sair): ");
             operation = getchar();
             clear_buffer();
 
@@ -38,14 +37,14 @@ int main(int argc, char *argv[]) {
                 printf("Insira o ID (0 a %d) do processador a ser usado: ", PROC_SIZE - 1);
                 scanf("%d", &proc_id);
                 clear_buffer();
-                if(proc_id < 0 || proc_id >= PROC_SIZE) {
+                if(proc_id < 0 && proc_id >= PROC_SIZE) {
                     printf("ID invalido, tente novamente...\n");
                     continue; // volta para o começo do loop
                 }
                 printf("Insira o endereco (0 a %d) de memoria: ", RAM_SIZE - 1);
                 scanf("%d", &ram_pos);
                 clear_buffer();
-                if(ram_pos < 0 || ram_pos >= RAM_SIZE) {
+                if(ram_pos < '0' && ram_pos >= RAM_SIZE) {
                     printf("Endereco invalido, tente movamente...\n");
                     continue; // volta para o começo do loop
                 }
@@ -55,14 +54,14 @@ int main(int argc, char *argv[]) {
                 printf("Insira o ID (0 a %d) do processador a ser usado: ", PROC_SIZE - 1);
                 scanf("%d", &proc_id);
                 clear_buffer();
-                if(proc_id < 0 || proc_id >= PROC_SIZE) {
+                if(proc_id < '0' && proc_id >= PROC_SIZE) {
                     printf("ID invalido, tente novamente...\n");
                     continue; // volta para o começo do loop
                 }
                 printf("Insira o endereco (0 a %d) de memoria: ", RAM_SIZE - 1);
                 scanf("%d", &ram_pos);
                 clear_buffer();
-                if(ram_pos < 0 || ram_pos >= RAM_SIZE) {
+                if(ram_pos < '0' && ram_pos >= RAM_SIZE) {
                     printf("Endereco invalido, tente movamente...\n");
                     continue; // volta para o começo do loop
                 }
@@ -84,6 +83,28 @@ int main(int argc, char *argv[]) {
                 printf("Saindo do programa...\n");
                 break;
 
+            } else if(operation == 'p') {
+                char memoria_escolha;
+                printf("Insiria o que deve ser printado (m = memoria, 0 a %d = ID do processador): ", PROC_SIZE - 1);
+                memoria_escolha = getchar();
+                clear_buffer();
+                char proc_size_char = '0' + PROC_SIZE;
+                if(memoria_escolha == 'm') {
+                    for(int i = 0; i < RAM_SIZE; i++) {
+                        printf("Endereco: %02d\n", memory.ram[i].line);
+                        print_data(memory.ram[i].data);
+                        printf("----------\n");
+                    }
+                } else if(memoria_escolha >= '0' && memoria_escolha < proc_size_char) {
+                    for(int i = 0; i < CACHE_SIZE; i++) {
+                        char memoria_escolha_string[2] = {'\0'};
+                        memoria_escolha_string[0] = memoria_escolha;
+                        printf("Processor %d, cache block: %d\n", atoi(memoria_escolha_string), i);
+                        print_state(processors[atoi(memoria_escolha_string)].cache.positions[i].state);
+                        print_data(processors[atoi(memoria_escolha_string)].cache.positions[i].data);
+                        printf("----------\n");
+                    }
+                }
             } else {
                 printf("Operacao invalida, tente novamente...\n");
             }
